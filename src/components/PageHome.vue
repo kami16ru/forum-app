7067ccc46200fa82056828a930209a298702748e
Kamil Khismatullin <kami16ru@gmail.com>
2021-03-28 01:32:58 +0300

    styles added

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>Hello from page home</div>
-  <div v-for="thread in threads" :key="thread.id">
-    <h2>{{ thread.title }}</h2>
-    <div v-for="postId in thread.posts" :key="postId">
-      <p>{{ userById(postById(postId).userId).name }}</p>
-      <p>{{ postById(postId).text }}</p>
+  <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+    <div class="post-list">
+
+      <div v-for="postId in thread.posts" :key="postId" class="post">
+
+        <div class="user-info">
+          <a href="#" class="user-name">{{ userNameByPostId(postId) }}</a>
+
+          <a href="#">
+            <img class="avatar-large" :src="userById(postById(postId).userId).avatar" alt="avatar">
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+
+        </div>
+
+        <div class="post-content">
+          <div>
+            <p>{{ postById(postId).text }}</p>
+          </div>
+        </div>
+
+        <div class="post-date text-faded">{{ postById(postId).publishedAt }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -27,6 +46,9 @@ export default {
     },
     userById (userId) {
       return this.users.find(u => u.id === userId)
+    },
+    userNameByPostId (postId) {
+      return this.userById(this.postById(postId).userId).name
     }
   }
 }
