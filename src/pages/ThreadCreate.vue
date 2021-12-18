e40ff418cd6572eb3657c5c5efa797047803171e
Kamil Khismatullin <kami16ru@gmail.com>
2021-12-18 11:45:27 +0300

    thread editor component

--- src/pages/ThreadCreate.vue
+++ src/pages/ThreadCreate.vue
@@ -3,50 +3,20 @@
 
     <h1>Create new thread in <i>{{ forum.name }}</i></h1>
 
-    <form @submit.prevent="save">
-      <div class="form-group">
-        <label for="thread_title">Title:</label>
-        <input v-model=title
-           type="text"
-           id="thread_title"
-           class="form-input"
-           name="title"
-        >
-      </div>
+    <ThreadEditor @save="save" @cancel="cancel" />
 
-      <div class="form-group">
-        <label for="thread_content">Content:</label>
-        <textarea
-          v-model="text"
-          id="thread_content"
-          class="form-input"
-          name="content"
-          rows="8"
-          cols="140"
-        ></textarea>
-      </div>
-
-      <div class="btn-group">
-        <button @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
-        <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
-      </div>
-    </form>
   </div>
 </template>
 <script>
 import { mapActions } from 'vuex'
+import ThreadEditor from '@/components/ThreadEditor'
 
 export default {
+  components: { ThreadEditor },
   props: {
     forumId: {
       type: String,
       required: true
-    }
-  },
-  data () {
-    return {
-      title: '',
-      text: ''
     }
   },
   computed: {
@@ -58,10 +28,10 @@ export default {
     ...mapActions({
       createThread: 'createThread'
     }),
-    async save () {
+    async save ({ title, text }) {
       const thread = await this.createThread({
-        title: this.title,
-        text: this.text,
+        title,
+        text,
         forumId: this.forum.id
       })
 
