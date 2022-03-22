dfca9ced38e664bfeccc2e5427ee73370a7ed7fc
Kamil Khismatullin <kami16ru@gmail.com>
2022-03-22 23:43:28 +0300

    Set up the Cloud Firestore Database

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
